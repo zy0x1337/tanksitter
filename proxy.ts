ec72099a5de5a2da5d8d99011552c9b1a55de070
zy0x1337 <zy0x@web.de>
2025-12-25 12:13:07 +0100

    fix§

--- proxy.ts
+++ proxy.ts
@@ -13,6 +13,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon\\.ico|manifest\\.json|icons/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
